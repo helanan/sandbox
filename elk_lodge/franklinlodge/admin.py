6e7123ed98dd4eb232517b988419882feb5ba30a
Helana <helanan@gmail.com>
2017-11-05 23:04:52 -0600

    fixed models and data a bit, need to rearrage structure and pks

--- elk_lodge/franklinlodge/admin.py
+++ elk_lodge/franklinlodge/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
-from .models import Elk_Member
+from .models import Elk_Member, Elk_Photos, Dues, Events, Elk_NewsLetter, Elk_Contact, Membership_Application
 
 # Register your models here.
 
 admin.site.register(Elk_Member)
+admin.site.register(Elk_Photos)
+admin.site.register(Dues)
+admin.site.register(Events)
+admin.site.register(Elk_NewsLetter)
+admin.site.register(Elk_Contact)
+admin.site.register(Membership_Application)
--- elk_lodge/franklinlodge/models.py
+++ elk_lodge/franklinlodge/models.py
@@ -12,23 +12,23 @@ class Elk_Member(models.Model):
 	elk_member_nickname = models.CharField(max_length=30)
 	elk_member_since_date = models.DateTimeField('date joined')
 
-	def __str__(self):
-		return self.elk_member_type
+	# def __str__(self):
+	# 	return self.elk_member_type
 
 	def __str__(self):
 		return self.elk_first_name
 	
-	def __str__(self):
-		return self.elk_last_name
+	# def __str__(self):
+	# 	return self.elk_last_name
 	
-	def __str__(self):
-		return self.elk_member_email_address
+	# def __str__(self):
+	# 	return self.elk_member_email_address
 	
-	def __str__(self):
-		return self.elk_member_nickname
+	# def __str__(self):
+	# 	return self.elk_member_nickname
 
-	def __str__(self):
-		return str(self.elk_member_since_date)
+	# def __str__(self):
+	# 	return str(self.elk_member_since_date)
 
 	def was_added_recently(self):
 		return self.elk_member_since_date >= timezone.now() - datetime.timedelta(days=1)
@@ -39,17 +39,17 @@ class Elk_Photos(models.Model):
 	photo_url = models.CharField(max_length=100)
 	photo_description = models.CharField(max_length=300)
 
-	def __str__(self):
-		return self.elk_member
+	# def __str__(self):
+	# 	return self.elk_member
 	
-	def __str__(self):
-		return self.photo_type
+	# def __str__(self):
+	# 	return self.photo_type
 	
 	def __str__(self):
 		return self.photo_url
 	
-	def __str__(self):
-		return self.photo_description
+	# def __str__(self):
+	# 	return self.photo_description
 
 class Dues(models.Model):
 	due_type = models.CharField(max_length=100)
@@ -58,8 +58,8 @@ class Dues(models.Model):
 	def __str__(self):
 		return self.due_type
 	
-	def __str__(self):
-		return self.due_amount
+	# def __str__(self):
+	# 	return str(self.due_amount)
 
 
 class Events(models.Model):
@@ -69,8 +69,8 @@ class Events(models.Model):
 	def __str__(self):
 		return self.event_type
 
-	def __str__(self):
-		return self.event_date
+	# def __str__(self):
+	# 	return str(self.event_date)
 
 
 class Elk_NewsLetter(models.Model):
@@ -83,26 +83,26 @@ class Elk_NewsLetter(models.Model):
 	archive_date = models.DateTimeField('elk newsletter date')
 
 
-	def __str__(self):
-		return self.new_subscriber
+	# def __str__(self):
+	# 	return self.new_subscriber
 
-	def __str__(self):
-		return self.subscriber_first_name
+	# def __str__(self):
+	# 	return self.subscriber_first_name
 	
-	def __str__(self):
-		return self.subscriber_last_name
+	# def __str__(self):
+	# 	return self.subscriber_last_name
 
 	def __str__(self):
 		return self.subscriber_email_address
 
-	def __str__(self):
-		return self.newsletter_title
+	# def __str__(self):
+	# 	return self.newsletter_title
 
-	def __str__(self):
-		return self.archive_urls
+	# def __str__(self):
+	# 	return self.archive_urls
 
-	def __str__(self):
-		return self.archive_date
+	# def __str__(self):
+	# 	return str(self.archive_date)
 
 class Elk_Contact(models.Model):
 	contact_first_name = models.CharField(max_length=20)
@@ -111,20 +111,20 @@ class Elk_Contact(models.Model):
 	inquiry_title = models.CharField(max_length=250)
 	inquiry_content = models.CharField(max_length=500)
 
-	def __str__(self):
-		return self.contact_first_name
+	# def __str__(self):
+	# 	return self.contact_first_name
 
-	def __str__(self):
-		return self.contact_last_name
+	# def __str__(self):
+	# 	return self.contact_last_name
 
 	def __str__(self):
 		return self.contact_email_address
 
-	def __str__(self):
-		return self.inquiry_title
+	# def __str__(self):
+	# 	return self.inquiry_title
 
-	def __str__(self):
-		return self.inquiry_content
+	# def __str__(self):
+	# 	return self.inquiry_content
 
 
 class Membership_Application(models.Model):
@@ -134,17 +134,17 @@ class Membership_Application(models.Model):
 	prospective_member_address = models.CharField(max_length=300)
 	prospective_membership_type = models.CharField(max_length=100)
 
-	def __str__(self):
-		return self.prospective_member_first_name
+	# def __str__(self):
+	# 	return self.prospective_member_first_name
 
-	def __str__(self):
-		return self.prospective_member_last_name
+	# def __str__(self):
+	# 	return self.prospective_member_last_name
 
 	def __str__(self):
 		return self.prospective_member_email_address
 	
-	def __str__(self):
-		return self.prospective_member_address
+	# def __str__(self):
+	# 	return self.prospective_member_address
 
-	def __str__(self):
-		return self.prospective_membership_type
+	# def __str__(self):
+	# 	return self.prospective_membership_type
